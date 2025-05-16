9f640a39c029a1fed7deef0907e75c85e83b03b6
Maxim Zhdanov <zhdanovmvd@gmail.com>
2025-05-16 09:34:41 +0400

    Add transfer action

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,31 +8,58 @@ class AccountsController < ApplicationController
     account = current_user.account
 
     account.with_lock do
-      account.balance += deposit_money
-      account.save!
+      account.update!(balance: account.balance + deposit_money)
     end
 
-    render json: { balance: account.balance }
+    render json: { balance: account.balance.amount }
   end
 
   def withdraw
     withdrawal_money = params[:amount].to_money
-    account = current_user.account
-
     return render json: { error: "Invalid amount" }, status: :bad_request if withdrawal_money.amount <= 0
 
+    account = current_user.account
     success = false
 
     account.with_lock do
       if account.balance >= withdrawal_money
-        account.balance -= withdrawal_money
-        account.save!
+        account.update!(balance: account.balance - withdrawal_money)
         success = true
       end
     end
 
     if success
-      render json: { balance: account.balance }
+      render json: { balance: account.balance.amount }
+    else
+      render json: { error: "Insufficient balance" }, status: :unprocessable_entity
+    end
+  end
+
+  def transfer
+    transfer_money = params[:amount].to_money
+    return render json: { error: "Invalid amount" }, status: :bad_request if transfer_money.amount <= 0
+
+    recipient = User.find_by(email: params[:recipient_email])
+    return render json: { error: "Recipient not found" }, status: :not_found unless recipient
+
+    from_account = current_user.account
+    to_account = recipient.account
+
+    accounts = [ from_account, to_account ].sort_by(&:id)
+    success = false
+
+    ActiveRecord::Base.transaction do
+      accounts.each { |account| account.lock! }
+
+      if from_account.balance >= transfer_money
+        from_account.update!(balance: from_account.balance - transfer_money)
+        to_account.update!(balance: to_account.balance + transfer_money)
+        success = true
+      end
+    end
+
+    if success
+      render json: { message: "Transfer successful", balance: from_account.balance.amount }
     else
       render json: { error: "Insufficient balance" }, status: :unprocessable_entity
     end
--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -99,4 +99,71 @@ RSpec.describe AccountsController, type: :controller do
       end
     end
   end
+
+  describe 'POST #transfer' do
+    let(:recipient) { create(:user).account }
+    let(:amount) { 50 }
+
+    context 'when sufficient balance' do
+      before { account.update!(balance: account.balance + 100.to_money) }
+
+      it 'deducts the amount from sender account and adds it to recipient account' do
+        initial_sender_balance = account.balance
+        initial_recipient_balance = recipient.balance
+
+        post :transfer, params: { recipient_email: recipient.user.email, amount: amount }
+
+        expect(account.reload.balance).to eq(initial_sender_balance - amount.to_money)
+        expect(recipient.reload.balance).to eq(initial_recipient_balance + amount.to_money)
+      end
+
+      it 'returns the updated sender balance in the response' do
+        post :transfer, params: { recipient_email: recipient.user.email, amount: amount }
+        parsed_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['balance'].to_money).to eq(account.reload.balance)
+      end
+    end
+
+    context 'when amount is 0 or less' do
+      let(:amount) { 0 }
+
+      it 'does not update any account balance' do
+        initial_sender_balance = account.balance
+        initial_recipient_balance = recipient.balance
+
+        post :transfer, params: { recipient_id: recipient.id, amount: amount }
+
+        expect(account.reload.balance).to eq(initial_sender_balance)
+        expect(recipient.reload.balance).to eq(initial_recipient_balance)
+      end
+
+      it 'returns a bad request status with an error message' do
+        post :transfer, params: { recipient_email: recipient.user.email, amount: amount }
+        parsed_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:bad_request)
+        expect(parsed_response['error']).to eq('Invalid amount')
+      end
+    end
+
+    context 'when the recipient is not found' do
+      it 'does not update the sender account balance' do
+        initial_sender_balance = account.balance
+
+        post :transfer, params: { recipient_email: "invalid email", amount: amount }
+
+        expect(account.reload.balance).to eq(initial_sender_balance)
+      end
+
+      it 'returns a not found status with an error message' do
+        post :transfer, params: { recipient_email: "invalid email", amount: amount }
+        parsed_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:not_found)
+        expect(parsed_response['error']).to eq('Recipient not found')
+      end
+    end
+  end
 end
