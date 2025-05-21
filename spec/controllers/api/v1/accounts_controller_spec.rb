c677c5e20706496f91f670de1b4ec983a7de5e7c
Maxim Zhdanov <zhdanovmvd@gmail.com>
2025-05-21 21:28:57 +0400

    Isolate specs using mocks

--- spec/controllers/api/v1/accounts_controller_spec.rb
+++ spec/controllers/api/v1/accounts_controller_spec.rb
@@ -22,34 +22,40 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
 
   describe 'POST #deposit' do
     let(:amount) { 100 }
+    let(:deposit_service) { instance_double(DepositMoney) }
 
-    it 'increases the user account balance by the specified amount' do
-      initial_balance = account.balance
-      post :deposit, params: { amount: amount }
-
-      expect(account.reload.balance).to eq(initial_balance + amount.to_money)
+    before do
+      allow(DepositMoney).to receive(:new).and_return(deposit_service)
     end
 
-    it 'returns the updated balance in the response' do
-      post :deposit, params: { amount: amount }
-      parsed_response = JSON.parse(response.body)
+    context "when deposit successful" do
+      before do
+        allow(deposit_service).to receive(:call).and_return({ success: true, message: 'Deposit successful', balance: amount })
+      end
 
-      expect(response).to have_http_status(:ok)
-      expect(parsed_response['balance'].to_money).to eq(account.reload.balance)
+      it 'returns the success message in the response' do
+        post :deposit, params: { amount: amount }
+        parsed_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['message']).to eq('Deposit successful')
+      end
+
+      it 'returns the updated balance in the response' do
+        post :deposit, params: { amount: amount }
+        parsed_response = JSON.parse(response.body)
+
+        expect(parsed_response['balance']).to eq(100)
+      end
     end
 
-    context 'when the amount is 0 or less' do
-      let(:amount) { 0 }
-
-      it 'does not update the account balance' do
-        initial_balance = account.balance
-        post :withdraw, params: { amount: amount }
-
-        expect(account.reload.balance).to eq(initial_balance)
+    context "when deposit failed" do
+      before do
+        allow(deposit_service).to receive(:call).and_return({ success: false, error: 'Invalid amount' })
       end
 
       it 'returns a bad request status with an error message' do
-        post :withdraw, params: { amount: amount }
+        post :withdraw, params: { amount: 0 }
         parsed_response = JSON.parse(response.body)
 
         expect(response).to have_http_status(:bad_request)
@@ -61,14 +67,23 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
   describe 'POST #withdraw' do
     let(:amount) { 50 }
 
-    context 'when the account has sufficient balance' do
-      before { account.update!(balance: account.balance + 100.to_money) }
+    let(:withdraw_service) { instance_double(WithdrawMoney) }
 
-      it 'reduces the user account balance by the specified amount' do
-        initial_balance = account.balance
+    before do
+      allow(WithdrawMoney).to receive(:new).and_return(withdraw_service)
+    end
+
+    context 'when withdrawal successful' do
+      before do
+        allow(withdraw_service).to receive(:call).and_return({ success: true, message: 'Withdrawal successful', balance: 100 })
+      end
+
+      it 'returns the success message in the response' do
         post :withdraw, params: { amount: amount }
+        parsed_response = JSON.parse(response.body)
 
-        expect(account.reload.balance).to eq(initial_balance - amount.to_money)
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['message']).to eq('Withdrawal successful')
       end
 
       it 'returns the updated balance in the response' do
@@ -76,45 +91,21 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
         parsed_response = JSON.parse(response.body)
 
         expect(response).to have_http_status(:ok)
-        expect(parsed_response['balance'].to_money).to eq(user.account.reload.balance)
+        expect(parsed_response['balance']).to eq(100)
       end
     end
 
-    context 'when the account has insufficient balance' do
-      let(:amount) { account.balance.amount + 100 }
-
-      it 'does not update the account balance' do
-        initial_balance = account.balance
-        post :withdraw, params: { amount: amount }
-
-        expect(account.reload.balance).to eq(initial_balance)
+    context 'when withdrawal failed' do
+      before do
+        allow(withdraw_service).to receive(:call).and_return({ success: false, error: 'Insufficient balance' })
       end
 
       it 'returns an error message in the response' do
-        post :withdraw, params: { amount: amount }
+        post :withdraw, params: { amount: 100 }
         parsed_response = JSON.parse(response.body)
 
         expect(response).to have_http_status(:unprocessable_entity)
         expect(parsed_response['error']).to eq('Insufficient balance')
-      end
-    end
-
-    context 'when the amount is 0 or less' do
-      let(:amount) { 0 }
-
-      it 'does not update the account balance' do
-        initial_balance = account.balance
-        post :withdraw, params: { amount: amount }
-
-        expect(account.reload.balance).to eq(initial_balance)
-      end
-
-      it 'returns a bad request status with an error message' do
-        post :withdraw, params: { amount: amount }
-        parsed_response = JSON.parse(response.body)
-
-        expect(response).to have_http_status(:bad_request)
-        expect(parsed_response['error']).to eq('Invalid amount')
       end
     end
   end
@@ -123,17 +114,23 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
     let(:recipient) { create(:user).account }
     let(:amount) { 50 }
 
-    context 'when sufficient balance' do
-      before { account.update!(balance: account.balance + 100.to_money) }
+    let(:transfer_service) { instance_double(TransferMoney) }
 
-      it 'deducts the amount from sender account and adds it to recipient account' do
-        initial_sender_balance = account.balance
-        initial_recipient_balance = recipient.balance
+    before do
+      allow(TransferMoney).to receive(:new).and_return(transfer_service)
+    end
 
+    context 'when transfer successful' do
+      before do
+        allow(transfer_service).to receive(:call).and_return({ success: true, message: 'Transfer successful', balance: 100 })
+      end
+
+      it 'returns the success message in the response' do
         post :transfer, params: { recipient_email: recipient.user.email, amount: amount }
+        parsed_response = JSON.parse(response.body)
 
-        expect(account.reload.balance).to eq(initial_sender_balance - amount.to_money)
-        expect(recipient.reload.balance).to eq(initial_recipient_balance + amount.to_money)
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['message']).to eq('Transfer successful')
       end
 
       it 'returns the updated sender balance in the response' do
@@ -141,47 +138,21 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
         parsed_response = JSON.parse(response.body)
 
         expect(response).to have_http_status(:ok)
-        expect(parsed_response['balance'].to_money).to eq(account.reload.balance)
+        expect(parsed_response['balance']).to eq(100)
       end
     end
 
-    context 'when amount is 0 or less' do
-      let(:amount) { 0 }
-
-      it 'does not update any account balance' do
-        initial_sender_balance = account.balance
-        initial_recipient_balance = recipient.balance
-
-        post :transfer, params: { recipient_id: recipient.id, amount: amount }
-
-        expect(account.reload.balance).to eq(initial_sender_balance)
-        expect(recipient.reload.balance).to eq(initial_recipient_balance)
+    context 'when transfer failed' do
+      before do
+        allow(transfer_service).to receive(:call).and_return({ success: false, error: 'Invalid amount' })
       end
 
       it 'returns a bad request status with an error message' do
-        post :transfer, params: { recipient_email: recipient.user.email, amount: amount }
+        post :transfer, params: { recipient_email: recipient.user.email, amount: 0 }
         parsed_response = JSON.parse(response.body)
 
         expect(response).to have_http_status(:bad_request)
         expect(parsed_response['error']).to eq('Invalid amount')
-      end
-    end
-
-    context 'when the recipient is not found' do
-      it 'does not update the sender account balance' do
-        initial_sender_balance = account.balance
-
-        post :transfer, params: { recipient_email: "invalid email", amount: amount }
-
-        expect(account.reload.balance).to eq(initial_sender_balance)
-      end
-
-      it 'returns a not found status with an error message' do
-        post :transfer, params: { recipient_email: "invalid email", amount: amount }
-        parsed_response = JSON.parse(response.body)
-
-        expect(response).to have_http_status(:not_found)
-        expect(parsed_response['error']).to eq('Recipient not found')
       end
     end
   end
